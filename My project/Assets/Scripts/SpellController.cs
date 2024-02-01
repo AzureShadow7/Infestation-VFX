b6144c57bbdb068d4206860389a25647b370be35
AzureShadow7 <106389001+AzureShadow7@users.noreply.github.com>
2024-02-01 23:27:03 +0000

    Added new unity input system

--- My project/Assets/Scripts/SpellController.cs
+++ My project/Assets/Scripts/SpellController.cs
@@ -5,28 +5,44 @@ using UnityEngine;
 public class SpellController : MonoBehaviour
 {
     public ParticleSystem Infestation;
+    public GameObject indicator;
+
+    public float indicatorSpeed = 2.0f;
+
     Vector3 spawnPoint = new Vector3(0.0f, 0.0f, 0.0f);
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //var spellEmission = Infestation.emission;
-        //spellEmission.enabled = false;
+        var spellEmission = Infestation.emission;
+        spellEmission.enabled = false;
+    }
+
+    void SpellPosition()
+    {
+        Instantiate(indicator, spawnPoint, Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //var spellEmission = Infestation.emission;
+        var spellEmission = Infestation.emission;
 
         if (Input.GetKeyDown(KeyCode.I))
         {
-            //spellEmission.enabled = true;
+            spellEmission.enabled = true;
             Instantiate(Infestation, spawnPoint, Quaternion.identity);
             Infestation.Play();
 
             Debug.Log("it's playing");
+
+            //SpellPosition();
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SpellPosition();
         }
     }
 }
